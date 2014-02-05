e112cbe722c734391556410dfb390c8d20069b4e
Mona Liu <liu.mona.c@gmail.com>
2014-02-05 14:29:52 -0800

    changed package

---
+++ gen/edu/ucsd/vis141/scifiapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.ucsd.vis141.scifiapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/ucsd/vis141/scifiapp/StartScreenActivity.java
@@ -0,0 +1,28 @@
+package edu.ucsd.vis141.scifiapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class StartScreenActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+        
+		return true;
+	}
+
+	public void startGame(View view) {
+		startActivity(new Intent(this, CameraActivity.class));
+	}
+}
